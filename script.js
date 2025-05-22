dbb3bf13c4729b4ab33bd3af4d74fa671510328b
alumno <alumno@Instituto.local>
2025-05-22 11:21:50 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
+//ejercicio 1
 let frutas = [ 'sandia', 'naranja','kiwi']
 
 
 frutas.push('banana');
+frutas.unshift('manzana')
 frutas.pop();
-let primero =frutas [0]
+frutas.shift()
+console.log(frutas.length)
+
+//ejercicio 2
+
+let nombresArreglo = ['maxi','guille','valen ']
+for (let index = 0; index < nombresArreglo.length; index++) {
+    nombresArreglo = nombresArreglo[index];
+    
+}
+console(nombres.length);
+nombresArreglo.push('sofia')
+nombresArreglo.push('grego')
+console.log(nombresArreglo)
+
+//ejercicio 3
 
