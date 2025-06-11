f903f6b94a4b263ad446bdb42d8cd3e07f94d8ce
alumno <alumno@Instituto.local>
2025-06-11 07:48:07 -0300

    .

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-
-//ejercicio 1
-let frutas = [ 'sandia', 'naranja','kiwi']
-
-
-frutas.push('banana');
-frutas.unshift('manzana')
-frutas.pop();
+let frutas = ['Pera', 'Manzana', 'Banana']
+frutas.push('Uva')
+frutas.unshift('Mango')
+frutas.pop()
 frutas.shift()
-console.log(frutas.length)
+console.log(frutas)
 
-//ejercicio 2
+//Ejercicio 2  
 
-let nombresArreglo = ['maxi','guille','valen ']
-for (let index = 0; index < nombresArreglo.length; index++) {
-    nombresArreglo = nombresArreglo[index];
-    
+let nombres = ['Facu', 'Benja', 'Alejo']
+console.log(nombres.length)
+nombres.push('Cristian','Luis')
+console.log(nombres.length)
+
+//Ejercicio 3  
+
+let num = [0, 1, 2, 3];
+for (let i = 0; i < num.length; i++) {
+  console.log(num[i]);
 }
-console(nombres.length);
-nombresArreglo.push('sofia')
-nombresArreglo.push('grego')
-console.log(nombresArreglo)
 
-//ejercicio 3
+//Ejercicio 4 
+
+let numeros = [2, 4, 10, 15]
+let suma = 0;
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i]; 
+}
+console.log("La suma total es:", suma);
+
+//Ejercicio 5 
+
+alert('A continuación se te pedirá que ingreses 5 palabras');
+let palabras = [];
+for (let i = 0; i < 5; i++) {
+
+  let palabra = prompt("Escribe una palabra:");
+  palabras.push(palabra);
+}
+console.log("Las palabras que escribiste son:", palabras);
 
